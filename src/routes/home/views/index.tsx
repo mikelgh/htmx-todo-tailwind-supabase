2cf1ae58479500cd1187ab424826555d4bb2cb97
Luzefiru <ian.deejay.iv@gmail.com>
2024-02-10 17:23:21 +0800

    feat: confirm Cloudflare Worker deployment & add skeleton loader

--- src/routes/home/views/index.tsx
+++ src/routes/home/views/index.tsx
@@ -2,11 +2,53 @@ import { Context } from 'hono';
 
 const home = async (c: Context) => {
   return c.render(
-    <div id="home-container" class="w-full px-4 mx-auto sm:px-6 lg:px-16">
-      <h3 class="mb-8 text-2xl font-bold text-gray-900 sm:text-3xl">
+    <div id="home-container" className="w-full px-4 mx-auto sm:px-6 lg:px-16">
+      <h3 className="mb-8 text-2xl font-bold text-gray-900 sm:text-3xl">
         Task List
       </h3>
-      <div hx-get="/tasks" hx-trigger="revealed" hx-swap="outerHTML" />
+      <div hx-get="/tasks" hx-trigger="load" hx-swap="outerHTML">
+        <div
+          role="status"
+          className="p-4 space-y-4 border border-gray-200 divide-y divide-gray-200 rounded shadow max-w htmx-indicator animate-pulse"
+        >
+          <div className="flex items-center justify-between">
+            <div>
+              <div className="h-2.5 bg-gray-300 rounded-full w-24 mb-2.5"></div>
+              <div className="w-32 h-2 bg-gray-200 rounded-full"></div>
+            </div>
+            <div className="h-2.5 bg-gray-300 rounded-full w-12"></div>
+          </div>
+          <div className="flex items-center justify-between pt-4">
+            <div>
+              <div className="h-2.5 bg-gray-300 rounded-full w-24 mb-2.5"></div>
+              <div className="w-32 h-2 bg-gray-200 rounded-full"></div>
+            </div>
+            <div className="h-2.5 bg-gray-300 rounded-full w-12"></div>
+          </div>
+          <div className="flex items-center justify-between pt-4">
+            <div>
+              <div className="h-2.5 bg-gray-300 rounded-full w-24 mb-2.5"></div>
+              <div className="w-32 h-2 bg-gray-200 rounded-full"></div>
+            </div>
+            <div className="h-2.5 bg-gray-300 rounded-full w-12"></div>
+          </div>
+          <div className="flex items-center justify-between pt-4">
+            <div>
+              <div className="h-2.5 bg-gray-300 rounded-full w-24 mb-2.5"></div>
+              <div className="w-32 h-2 bg-gray-200 rounded-full"></div>
+            </div>
+            <div className="h-2.5 bg-gray-300 rounded-full w-12"></div>
+          </div>
+          <div className="flex items-center justify-between pt-4">
+            <div>
+              <div className="h-2.5 bg-gray-300 rounded-full w-24 mb-2.5"></div>
+              <div className="w-32 h-2 bg-gray-200 rounded-full"></div>
+            </div>
+            <div className="h-2.5 bg-gray-300 rounded-full w-12"></div>
+          </div>
+          <span className="sr-only">Loading...</span>
+        </div>
+      </div>
     </div>
   );
 };
